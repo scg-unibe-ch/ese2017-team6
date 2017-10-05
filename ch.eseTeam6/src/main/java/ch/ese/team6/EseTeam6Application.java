d90ac3fd2159404c632835dde6949576f5c8f22a
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2017-10-05 11:00:59 +0200

    Initialize Setup

---
+++ ch.eseTeam6/src/main/java/ch/ese/team6/EseTeam6Application.java
@@ -0,0 +1,12 @@
+package ch.ese.team6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EseTeam6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EseTeam6Application.class, args);
+	}
+}
---
+++ ch.eseTeam6/src/main/java/ch/ese/team6/MvcConfigs.java
@@ -0,0 +1,17 @@
+package ch.ese.team6;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfigs extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+	}
+	
+}
