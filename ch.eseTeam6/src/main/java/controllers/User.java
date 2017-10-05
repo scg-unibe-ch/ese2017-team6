8c700c35204231ae90a181f9ce389b65671d9219
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2017-10-05 12:03:54 +0200

    Added Users Model and User Controller

---
+++ ch.eseTeam6/src/main/java/controllers/User.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class User {
+	
+	public void showallUsers() {
+		//Get all Users
+		//Post userIndex.html (allUsers)
+		
+	}
+	
+	public void createUser() {
+		
+	}
+
+}
