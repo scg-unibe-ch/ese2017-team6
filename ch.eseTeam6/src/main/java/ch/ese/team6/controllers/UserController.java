a30def71f276e1f07b190fdd1abb3310cc335032
Domi <Domi@Domi-PC>
2017-10-09 16:05:14 +0200

    CodeCleanUp

--- ch.eseTeam6/src/main/java/ch/ese/team6/controllers/UserController.java
+++ ch.eseTeam6/src/main/java/ch/ese/team6/controllers/UserController.java
@@ -1,6 +1,6 @@
 package ch.ese.team6.controllers;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import ch.ese.team6.models.users.UserRepository;
