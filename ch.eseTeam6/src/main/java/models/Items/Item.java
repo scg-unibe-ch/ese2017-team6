587ca90f6df64ab17c53005f4a445a577fabd951
Mauro Kiener <mauro.kiener@students.unibe.ch>
2017-10-10 13:53:34 +0200

    merge1

---
+++ ch.eseTeam6/src/main/java/models/Items/Item.java
@@ -0,0 +1,28 @@
+package models.Items;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * All Items from the library of the firm
+ * @author mauro
+ *
+ */
+@Entity
+public class Item {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+ 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
