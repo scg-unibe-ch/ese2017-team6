63884888810666aa29e09096d61f9b01bec140d9
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2017-10-10 14:04:46 +0200

    little adjustments in order files

--- ch.eseTeam6/src/main/java/models/Order.java
+++ ch.eseTeam6/src/main/java/models/Order.java
@@ -1,10 +1,25 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Order {
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
     private String customerName; // change to Customer objects
     private String deliveryAddress; // change to Address objects
     
+    public Integer getId() {
+    	return id;    	
+    }
+    
+    public void setId(Integer id) {
+    	this.id = id;
+    }
 
     public String getCustomerName() {
         return customerName;
