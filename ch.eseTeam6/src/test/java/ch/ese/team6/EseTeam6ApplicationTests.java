7d57be5517e2784d22bf8ce93cd3b3ff69358089
Domi <Domi@Domi-PC>
2017-10-11 13:36:38 +0200

    Added First Test

--- ch.eseTeam6/src/test/java/ch/ese/team6/EseTeam6ApplicationTests.java
+++ ch.eseTeam6/src/test/java/ch/ese/team6/EseTeam6ApplicationTests.java
@@ -1,9 +1,14 @@
 package ch.ese.team6;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import ch.ese.team6.models.users.UserRepository;
+import ch.ese.team6.models.users.Users;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,18 @@ public class EseTeam6ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+	@Autowired
+	public void UsersShouldBeSuccessToSave() {
+		long count = userRepository.count();
+		Users user = new Users("username","anzeigename", "password" );
+		userRepository.save(user);
+		Assert.assertTrue(userRepository.count()+1 == count);
 	}
 
 }
